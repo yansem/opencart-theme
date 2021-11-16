2c8141c30063dca68db02e84d290f5ed78968f9b
SEMA <janchevsem@mail.ru>
2021-11-16 12:47:27 +0500

    cart modal ajax

--- catalog/view/theme/ysashop/js/common.js
+++ catalog/view/theme/ysashop/js/common.js
@@ -144,29 +144,31 @@ var cart = {
 			data: 'product_id=' + product_id + '&quantity=' + (typeof(quantity) != 'undefined' ? quantity : 1),
 			dataType: 'json',
 			beforeSend: function() {
-				$('#cart > button').button('loading');
+				// $('#cart > button').button('loading');
 			},
 			complete: function() {
-				$('#cart > button').button('reset');
+				// $('#cart > button').button('reset');
 			},
 			success: function(json) {
-				$('.alert-dismissible, .text-danger').remove();
+				// $('.alert-dismissible, .text-danger').remove();
 
 				if (json['redirect']) {
 					location = json['redirect'];
 				}
 
 				if (json['success']) {
-					$('#content').parent().before('<div class="alert alert-success alert-dismissible"><i class="fa fa-check-circle"></i> ' + json['success'] + ' <button type="button" class="close" data-dismiss="alert">&times;</button></div>');
+					$('.count-items').html(json['total']);
+					/*$('#content').parent().before('<div class="alert alert-success alert-dismissible"><i class="fa fa-check-circle"></i> ' + json['success'] + ' <button type="button" class="close" data-dismiss="alert">&times;</button></div>');
 
 					// Need to set timeout otherwise it wont update the total
 					setTimeout(function () {
 						$('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
 					}, 100);
 
-					$('html, body').animate({ scrollTop: 0 }, 'slow');
+					$('html, body').animate({ scrollTop: 0 }, 'slow');*/
 
-					$('#cart > ul').load('index.php?route=common/cart/info ul li');
+					$('#cart-modal .modal-body').load('index.php?route=common/cart/info .in-cart');
+					new bootstrap.Modal(document.getElementById('cart-modal')).show();
 				}
 			},
 			error: function(xhr, ajaxOptions, thrownError) {
@@ -216,6 +218,7 @@ var cart = {
 				$('#cart > button').button('reset');
 			},
 			success: function(json) {
+				$('.count-items').html(json['total']);
 				// Need to set timeout otherwise it wont update the total
 				setTimeout(function () {
 					$('#cart > button').html('<span id="cart-total"><i class="fa fa-shopping-cart"></i> ' + json['total'] + '</span>');
@@ -224,7 +227,7 @@ var cart = {
 				if (getURLVar('route') == 'checkout/cart' || getURLVar('route') == 'checkout/checkout') {
 					location = 'index.php?route=checkout/cart';
 				} else {
-					$('#cart > ul').load('index.php?route=common/cart/info ul li');
+					$('#cart-modal .modal-body').load('index.php?route=common/cart/info .in-cart');
 				}
 			},
 			error: function(xhr, ajaxOptions, thrownError) {
